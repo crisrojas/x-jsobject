13015825b3dcd92a048110c13e16eaa0ee57937a
Cristian Rojas <cristian.rojas@live.fr>
2025-05-08 20:19:53 +0200

    Increment calls `onUpdate` callback and updates count

--- Tests/Code.swift
+++ Tests/Code.swift
@@ -1,60 +1,52 @@
 import JavaScriptCore
 import XCTest
 
+final class JSObject {
+    private let context: JSContext
+    private let jsValue: JSValue
+    private var callbacks = [String: Any]()
 
-class Tests: XCTestCase {
-    struct ViewModel {
-        let onUpdate: (Int) -> Void
-        
-        private let jsContext: JSContext
-        private let viewModel: JSValue
-
-        func increment() {
-            viewModel.invokeMethod("increment", withArguments: [])
-        }
-        
-        let jsSource = """
-            function ViewModel() {
-                this.counter = 0;
-                this.onUpdate = null;
-            }
-            
-            ViewModel.prototype.increment = function () {
-                this.counter += 1;
-                this.onUpdate(this.counter);
-            };
-            """
-        
-        init(onUpdate: @escaping (Int) -> Void) {
-            self.onUpdate = onUpdate
-
-            let context = JSContext()!
-            context.evaluateScript(jsSource)
-
-            let vmConstructor = context.objectForKeyedSubscript("ViewModel")!
-            let viewModel = vmConstructor.construct(withArguments: [])!
-
-            let onUpdateBlock: @convention(block) (Int) -> Void = { value in
-                onUpdate(value)
-            }
-
-            viewModel.setObject(onUpdateBlock, forKeyedSubscript: "onUpdate" as NSString)
-
-            self.jsContext = context
-            self.viewModel = viewModel
-        }
+    init(js: String, key: String) {
+        self.context = JSContext()!
+        context.evaluateScript(js)
+        let constructor = context.objectForKeyedSubscript(key)!
+        self.jsValue = constructor.construct(withArguments: [])!
     }
-    
-    func test_init_mapsViewModelOnUpateAndIncrementMethod() {
+
+    func set(_ callbackName: String, callback: @escaping (Any) -> Void) {
+        let block: @convention(block) (JSValue) -> Void = { value in
+            callback(value.toObject()!)
+        }
+        
+        callbacks[callbackName] = block
+        jsValue.setObject(block, forKeyedSubscript: callbackName as NSString)
+    }
+
+    func invoke(_ methodKey: String, args: [Any] = []) {
+        _ = jsValue.invokeMethod(methodKey, withArguments: args)
+    }
+}
+
+final class Tests: XCTestCase {
+    func test_increment_callsOnUpdateCallbackWithUpdatedValue() {
         var count = 0
-        
-        let e = expectation(description: "Callback triggered")
-        let viewModel = ViewModel(
-            onUpdate: { count = $0 ; e.fulfill() }
-        )
-        
-        viewModel.increment()
+        let object = JSObject(js: jsSource, key: "ViewModel")
+        let e = expectation(description: "Wait for callback")
+        object.set("onUpdate") { count = $0 as! Int ; e.fulfill() }
+        object.invoke("increment")
         wait(for: [e], timeout: 1)
         XCTAssertEqual(count, 1)
     }
+
+    let jsSource = """
+        function ViewModel() {
+            this.counter = 0;
+            this.onUpdate = null;
+        }
+
+        ViewModel.prototype.increment = function () {
+            this.counter += 1;
+            this.onUpdate(this.counter);
+        };
+        """
 }
