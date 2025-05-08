f5f9ecf4347038694e3ba8eb4628633dab846161
Cristian Rojas <cristian.rojas@live.fr>
2025-05-08 19:19:13 +0200

    Add test target

---
+++ Tests/Code.swift
@@ -0,0 +1,12 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 8/5/25.
+
+import JavaScriptCore
+import XCTest
+
+class Tests: XCTestCase {
+    func test() {
+        XCTExpectFailure {
+            XCTFail()
+        }
+    }
+}
